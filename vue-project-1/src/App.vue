349eb0b7e57c368ed75925b15998e73318b7f995
ganesh <ganesanhere4u@gmail.com>
2019-10-25 23:59:52 +0530

    ganesh--test

---
+++ vue-project-1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+<router-link to="/">Home</router-link>
+<router-link to="/login">login</router-link>
+  <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
